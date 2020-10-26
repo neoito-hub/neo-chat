06dab3f56114204875e24c017746e76578d0bd77
that-coder <thatcoder.space@gmail.com>
2020-10-26 19:13:57 +0530

    Added shellscript for automation

--- run.sh
+++ run.sh
@@ -1,1 +1,21 @@
-cd server
+echo "Starting installation..."
+echo "App requires Postgres and Redis for handling chat Data"
+
+// check yes/no
+
+
+// install Postgres
+
+
+// setup caddy
+
+
+// ask for domain
+
+
+
+//point ( proxy) caddy to go and react Ports 
+
+
+// neochat.com to react build directory and neochat/api/ to go binary
+
